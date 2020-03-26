4ffc728cee92c85b2de72f1a5deb90e447187ed0
James Jensen <jnjensen@live.com>
2020-03-26 02:58:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,31 @@ def display_board(array)
   puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
 
+def position_taken?(board, index)
+  x = board[index]
+  if x == " " || x == "" || x == nil
+    return false
+  elsif x == "X" || x == "O"
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  y = board[index]
+  if y >= 0 && y <= 8
+    if !(position_taken?(board, index))
+      return true
+    end
+  end
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+  return board
+end
+
+
+  
+  
+  
+  
